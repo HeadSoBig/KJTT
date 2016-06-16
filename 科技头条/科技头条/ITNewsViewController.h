72e3bfbb6f246704ce8299d34a80078bb25d0ce9
wangJunBo <wangJunBo@qq.com>
2016-06-17 01:23:19 +0800

    创建列表控制器,设置cellId

---
+++ 科技头条/科技头条/ITNewsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ITNewsViewController.h
+//  科技头条
+//
+//  Created by 王军波 on 16/6/17.
+//  Copyright © 2016年 王军波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ITNewsViewController : UITableViewController
+
+@end
