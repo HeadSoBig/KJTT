49405b8f2d1567557cb0b8f093891482fb7297d4
wangJunBo <wangJunBo@qq.com>
2016-06-17 00:56:42 +0800

    初始化项目

---
+++ 科技头条/科技头条/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  科技头条
+//
+//  Created by 王军波 on 16/6/17.
+//  Copyright © 2016年 王军波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
